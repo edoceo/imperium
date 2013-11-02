abcd941cd8831b943db6b94ee86318b51005f1b5
edoceo <code@edoceo.com>
2013-11-02 10:33:39 -0700

    Search Update works

--- bin/cron-search-update.php
+++ bin/cron-search-update.php
@@ -11,7 +11,7 @@
 require_once(dirname(dirname(__FILE__)) . '/lib/cli.php');
 
 // We Re-Create the Table Each Time :(
-$db->query('DROP TABLE full_text');
+radix_db_sql::query('DROP TABLE full_text');
 
 $sql = 'CREATE TABLE full_text ( ';
 $sql.= ' link_to varchar(32) not null, ';
@@ -19,9 +19,9 @@ $sql.= ' link_id int not null, ';
 $sql.= ' name varchar(256), ';
 $sql.= ' ft text, ';
 $sql.= ' tv tsvector )';
-$db->query($sql);
+radix_db_sql::query($sql);
 
-$db->query('CREATE INDEX full_text_tv_idx ON full_text USING gin(tv)');
+radix_db_sql::query('CREATE INDEX full_text_tv_idx ON full_text USING gin(tv)');
 
 // List of Table to Index
 $tab_list = array(
@@ -90,6 +90,6 @@ foreach ($tab_list as $tab => $tab_spec) {
     $sql.= " FROM $tab";
 
     // echo "$sql\n";
-    $db->query($sql);
+    radix_db_sql::query($sql);
 }
 
