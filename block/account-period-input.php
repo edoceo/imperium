f12b34ef85ff6e223ffaaf2357c32ce66b39da3e
edoceo <code@edoceo.com>
2014-01-04 23:53:11 -0800

    fix date pick

--- block/account-period-input.php
+++ block/account-period-input.php
@@ -10,10 +10,10 @@
 
 echo '<div class="bf c">';
 
-echo radix_html_form::select('m', radix::$view->Month, null, radix::$view->MonthList); //null,$this->data['month'],null,false); // Month
-echo radix_html_form::select('y', radix::$view->Year, null, radix::$view->YearList ); //null,$this->data['year'],null,false); // Year
-echo radix_html_form::select('p', radix::$view->Period, null, radix::$view->PeriodList); //null,$this->data['period'],null,false); // Period
-echo radix_html_form::select('c', 'View');
+echo radix_html_form::select('m', radix::$view->Month, radix::$view->MonthList); //null,$this->data['month'],null,false); // Month
+echo radix_html_form::select('y', radix::$view->Year, radix::$view->YearList ); //null,$this->data['year'],null,false); // Year
+echo radix_html_form::select('p', radix::$view->Period, radix::$view->PeriodList); //null,$this->data['period'],null,false); // Period
+echo radix_html_form::submit('c', 'View');
 
 echo '<div class="bf c">';
 echo '<label for="ex_close">&nbsp;' . radix_html_form::checkbox('ex_close', radix::$view->ExcludeClose, null, array('Checked'=>1,'unChecked'=>0)) . '&nbsp;Exclude Closing Transactions</label>';
