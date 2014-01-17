4f17602fe009c131deca334177fa050a9b1308e0
edoceo <code@edoceo.com>
2014-01-17 09:40:10 -0800

    CLI handling

--- lib/cli.php
+++ lib/cli.php
@@ -5,8 +5,7 @@
 
 
 // Configurations
-
-$cli_opt = getopt('c:d:s',array('config:','date:','diff:','send'));
+$cli_opt = getopt('c:d:ms',array('config:','date:','diff:','mail','send'));
 
 if (empty($cli_opt['config']) && !empty($cli_opt['c'])) $cli_opt['config'] = $cli_opt['c'];
 if (!empty($cli_opt['config'])) putenv('IMPERIUM_CONFIG=' . $cli_opt['config']);
@@ -16,23 +15,18 @@ if (!empty($cli_opt['d'])) $cli_opt['date'] = $cli_opt['d'];
 if (!empty($cli_opt['date'])) $time = strtotime($cli_opt['date']);
 $date = strftime('%Y-%m-%d',$time);
 
+if (isset($cli_opt['m']) || isset($cli_opt['mail'])) $cli_opt['mail'] = true;
 if (isset($cli_opt['s']) || isset($cli_opt['send'])) $cli_opt['send'] = true;
 
 // Bootstrapper
 require_once(dirname(dirname(__FILE__)) . '/boot.php');
 
-// $db = Zend_Registry::get('db');
-
-// $za = Zend_Auth::getInstance();
-// $za->authenticate(new App_Auth($_ENV['cron']['username'],$_ENV['cron']['password']));
-// $cu = $za->getIdentity();
-// if (empty($cu)) {
-//     die("Invalid Username or Password");
-// }
-
 function send_mail($rcpt, $mail)
 {
 	$uri = parse_url($_ENV['mail']['smtp']);
+
+	$mail = str_replace('%head_from%', sprintf('"%s" <%s>', $_ENV['company']['name'], $_ENV['mail']['from']), $mail);
+	$mail = str_replace('%head_hash%', md5(openssl_random_pseudo_bytes(256)) . '@' . parse_url($_ENV['application']['base'], PHP_URL_HOST), $mail);
 
 	require_once('Radix/mail/smtp.php');
 	$smtp = new radix_mail_smtp(sprintf('%s://%s:%d', $uri['scheme'], $uri['host'], $uri['port']));
