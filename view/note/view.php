b028f80374291ad203d495ff54c0edffb8c7a158
edoceo <code@edoceo.com>
2015-03-13 16:28:08 -0700

    Use Parsedown

--- view/note/view.php
+++ view/note/view.php
@@ -9,13 +9,13 @@ namespace Edoceo\Imperium;
 use Edoceo\Radix\Radix;
 use Edoceo\Radix\HTML\Form;
 
-require_once('markdown.php');
-
 $this->Note = new Base_Note($_GET['id']);
 $_ENV['title'] = $this->Note['name'];
 
+$P = new \Parsedown();
+
 echo '<div class="markdown" style="background:#e0e0e0;border:2px inset #666;padding:8px;">';
-echo markdown($this->Note['data']);
+echo $P->text($this->Note['data']);
 echo '</div>';
 
 echo '<form action="' . Radix::link('/note/save') . '" id="note-edit-form" method="post">';
