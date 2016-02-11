27aaf4f1faa3c2416642d00f331507f7698ec8e5
edoceo <code@edoceo.com>
2016-02-11 07:01:36 -0800

    Add Account button restored

--- view/contact/view.php
+++ view/contact/view.php
@@ -31,7 +31,7 @@ form div > label {
 }
 </style>
 
-<form action="<?= Radix::link('/contact/save?c=' . $this->Contact['id']) ?>" method="post">
+<form action="<?= Radix::link('/contact/save?c=' . $this->Contact['id']) ?>" autocomplete="off" method="post">
 
 <div>
 <?= Form::hidden('id',$this->Contact['id']); ?>
@@ -47,6 +47,7 @@ form div > label {
 <div class="fi">
 	<label>Company:</label>
 	<?= Form::text('company',$this->Contact['company'])?>
+	<?= Form::hidden('parent_id', $this->Contact['parent_id']) ?>
 <?php
 if (!empty($this->Contact['parent_id'])) {
     echo ' <a href="' . Radix::link('/contact/view?c=' . $this->Contact['parent_id']) . '"><i class="fa fa-bolt"></i></a>';
@@ -128,17 +129,33 @@ if (!empty($this->ContactChannelList)) {
 // echo '<tr><td class="l">Google:</td><td colspan="3"><div id="contact-google-area"><input id="contact-google-view" type="button" value="View" ></div></td></tr>';
 
 echo '<div class="form-controls">';
-echo '<button class="exec" name="a" type="submit" value="capture">Photo</button>';
-echo '<button class="exec" id="exec-contact-ping" type="button" value="ping">Ping</button>';
 echo '<button class="good" name="a" type="submit" value="save">Save</button>';
-echo '<button class="fail" name="a" type="submit" value="delete">Delete</button>';
-//echo '<a class="button" href="' . Radix::link('/contact/merge?' . http_build_query(array('c' => $this->Contact['id']))) . '">Merge</a>';
+// echo '<button class="exec" id="exec-contact-ping" type="button" value="ping">Ping</button>';
+// echo '<button class="exec" name="a" type="submit" value="capture">Photo</button>';
+// echo '<button class="fail" name="a" type="submit" value="delete">Delete</button>';
+// echo '<a class="button" href="' . Radix::link('/contact/merge?' . http_build_query(array('c' => $this->Contact['id']))) . '">Merge</a>';
 echo '</div>';
 echo '</form>';
 
 ?>
 <script>
 $(function() {
+
+    $("#contact").autocomplete({
+        source: "<?= Radix::link('/contact/ajax?field=contact'); ?>",
+        focus: function(event, ui) {
+            if (ui.item) {
+                $("#contact").val(ui.item.contact);
+                event.preventDefault();
+            }
+        },
+        select: function(event, ui) {
+            if (!ui.item) return;
+            $("#contact").val(ui.item.contact);
+            event.preventDefault();
+        }
+    });
+
     $("#company").autocomplete({
         source: "<?= Radix::link('/contact/ajax?field=company'); ?>",
         change: function(event, ui) {
@@ -156,20 +173,7 @@ $(function() {
 			$("#company").val(ui.item.company);
 		}
     });
-    $("#contact").autocomplete({
-        source: "<?= Radix::link('/contact/ajax?field=contact'); ?>",
-        focus: function(event, ui) {
-            if (ui.item) {
-                $("#contact").val(ui.item.contact);
-                event.preventDefault();
-            }
-        },
-        select: function(event, ui) {
-            if (!ui.item) return;
-            $("#contact").val(ui.item.contact);
-            event.preventDefault();
-        }
-    });
+
     // $("#kind").autocomplete({
     //     source: "<?= Radix::link('/contact/ajax?field=kind'); ?>"
     // });
@@ -311,8 +315,13 @@ if ($this->InvoiceList) {
 echo '<div>';
 echo '<h2>Accounts: </h2>';
 if (empty($this->Account['id'])) {
-	echo 'Receiveable:';
-	echo '<button class="exec" name="a" title="Create new Account for this Contact" style="margin:4px;" type="submit" value="create-account">Create</button>';
+?>
+	<form action="<?= Radix::link('/contact/save?c=' . $this->Contact['id']) ?>" autocomplete="off" method="post">
+	<div>
+	<p>Receiveable: <button class="exec" name="a" title="Create new Account for this Contact" style="margin:4px;" type="submit" value="create-account">Create</button>
+	</div>
+	</form>
+<?php
 } else {
 	echo '<a href="' . Radix::link('/account/ledger?' . http_build_query(array('id' => $this->Account['id']))) . '">Account</a>:';
 	echo '<span style="font-size: 22px; line-height: 32px;">';
