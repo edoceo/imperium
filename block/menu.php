a44a970cebe101561f9714a4d8425fcd10e1def3
edoceo <code@edoceo.com>
2017-09-18 17:39:19 +0000

    begin BS4

--- block/menu.php
+++ block/menu.php
@@ -30,19 +30,72 @@ if (!ACL::may('/block/menu')) {
 
 ?>
 
-<nav class="one">
-<ul class="menu">
-<li><a href="<?= Radix::link('/') ?>" title="Dashboard"><i class="fa fa-home"></i></a>
+<style>
+nav li {
+	position: relative;
+}
+
+li.dropdown-hook:active > ul,
+li.dropdown-hook:focus > ul,
+li.dropdown-hook:hover > ul {
+	display: block;
+}
+
+ul.dropdown-menu {
+	/* border: 1px outset #000; */
+	display: none;
+	left: 0;
+	min-width: 15em;
+	position: absolute;
+	top: 98%;
+	z-index: 20;
+}
+/*
+li.dropdown-hook {
+	font-weight: 700;
+	margin: 0;
+	padding: 0;
+	position: relative;
+
+	a {
+		display: block;
+		font-size: 120%;
+		padding: 0.125em 0.5em;
+		text-decoration: none;
+		white-space: nowrap;
+	}
+
+	a:hover {
+		background: @high_bg;
+		color: @body_bg;
+	}
+}
+*/
+</style>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+<a class="navbar-brand" href="<?= Radix::link('/') ?>" title="Home"><i class="fa fa-home"></i></a>
+
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	<span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+<ul class="navbar-nav">
+<li class="nav-item">
+<!--
 	<ul>
 	<?php
 	// Show MRU
-	if (!empty($_SESSION['mru']) && (count($_SESSION['mru']) > 0)) {
-		$mru = array_reverse($_SESSION['mru']);
-		foreach ($mru as $key => $val) {
-			echo '<li><a href="' . Radix::link($val['link']) . '">' . $val['html'] . '</a></li>';
-		}
-		echo '<li><hr /></li>';
-	}
+	// if (!empty($_SESSION['mru']) && (count($_SESSION['mru']) > 0)) {
+	// 	$mru = array_reverse($_SESSION['mru']);
+	// 	foreach ($mru as $key => $val) {
+	// 		echo '<li><a href="' . Radix::link($val['link']) . '">' . $val['html'] . '</a></li>';
+	// 	}
+	// 	echo '<li><hr /></li>';
+	// }
 	?>
 	<li><a class="fancybox fancybox.ajax" href="<?= Radix::link('/note/edit?l=r') ?>"><i class="fa fa-file-text-o"></i> New Note</a></li>
 	<li><a class="fancybox fancybox.ajax" href="<?= Radix::link('/file/edit?l=r') ?>"><i class="fa fa-file"></i> New File</a></li>
@@ -58,41 +111,45 @@ if (!ACL::may('/block/menu')) {
 	echo '<li><a href="' . Radix::link('/auth/sign-out') . '"><i class="fa fa-sign-out"></i> Sign Out</a></li>';
 	?>
 	</ul>
+-->
 </li>
 
 <!-- Contacts -->
-<li><a href="<?= Radix::link('/contact') ?>"><i class="fa fa-users"></i> Contacts</a>
-<ul>
-<?php
-if (!empty($_ENV['contact']['id'])) {
-	echo '<li><a href="' . Radix::link('/contact/view?c=' . $_ENV['contact']['id']) . '"><i class="fa fa-user"></i> ' . $_ENV['contact']['name'] . '</a></li>';
-	echo '<li><hr /></li>';
-	echo '<li><a href="' . Radix::link('/contact/channel?a=create&amp;c=' . $_ENV['contact']['id']) . '"><i class="fa fa-plus"></i> Add Channel</a></li>';
-	// echo '<li><a href="' . Radix::link('/task/edit/link/' . $this->Contact->link() . '">' . img('/silk/1.3/note_add.png','Add Task') . ' Add Task</a></li>';
-	// echo '<li><a href="' . Radix::link('/history/edit/link/' . $this->Contact->link() . '">' . img('/silk/1.3/note_add.png','Add Note') . ' Add History</a></li>';
-	//$menu[] = array('/timer/edit',$html->image('clock_add.png').' Clean');
-	//$menu[] = array('/timer/edit',$html->image('clock_add.png').' Search');
-	//$menu[] = array('/timer/edit',$html->image('clock_add.png').' Lists');
-	//$menu[] = array('/timer/edit',$html->image('clock_add.png').' Help');
-	echo '<li><hr /></li>';
-}
-?>
-<li><a href="<?= Radix::link('/contact/view') ?>"><i class="fa fa-plus"></i> Create</a></li>
-<li><a href="<?= Radix::link('/contact?kind=contacts')  ?>"><i class="fa fa-users"></i> Contacts</a></li>
-<li><a href="<?= Radix::link('/contact?kind=companies') ?>"><i class="fa fa-building"></i> Companies</a></li>
-<li><a href="<?= Radix::link('/contact?kind=vendors') ?>"><i class="fa fa-truck"></i> Vendors</a></li>
-<li><a href="<?= Radix::link('/contact/labels') ?>"><i class="fa fa-file-text-o"></i> Mail Labels</a></li>
-<li><hr></li>
-<li><a href="<?= Radix::link('/contact/import') ?>"><i class="fa fa-cloud-upload"></i> Import</a></li>
-<li><a href="<?= Radix::link('/contact/sync') ?>"><i class="fa fa-refresh"></i> Sync</a></li>
-</ul>
+<li class="nav-item dropdown-hook">
+	<a class="nav-link" href="<?= Radix::link('/contact') ?>"><i class="fa fa-users"></i> Contacts</a>
+	<ul class="dropdown-menu">
+	<?php
+	if (!empty($_ENV['contact']['id'])) {
+		echo '<li><a href="' . Radix::link('/contact/view?c=' . $_ENV['contact']['id']) . '"><i class="fa fa-user"></i> ' . $_ENV['contact']['name'] . '</a></li>';
+		echo '<li><hr /></li>';
+		echo '<li><a href="' . Radix::link('/contact/channel?a=create&amp;c=' . $_ENV['contact']['id']) . '"><i class="fa fa-plus"></i> Add Channel</a></li>';
+		// echo '<li><a href="' . Radix::link('/task/edit/link/' . $this->Contact->link() . '">' . img('/silk/1.3/note_add.png','Add Task') . ' Add Task</a></li>';
+		// echo '<li><a href="' . Radix::link('/history/edit/link/' . $this->Contact->link() . '">' . img('/silk/1.3/note_add.png','Add Note') . ' Add History</a></li>';
+		//$menu[] = array('/timer/edit',$html->image('clock_add.png').' Clean');
+		//$menu[] = array('/timer/edit',$html->image('clock_add.png').' Search');
+		//$menu[] = array('/timer/edit',$html->image('clock_add.png').' Lists');
+		//$menu[] = array('/timer/edit',$html->image('clock_add.png').' Help');
+		echo '<li><hr /></li>';
+	}
+	?>
+	<li><a href="<?= Radix::link('/contact/view') ?>"><i class="fa fa-plus"></i> Create</a></li>
+	<li><a href="<?= Radix::link('/contact?kind=contacts')  ?>"><i class="fa fa-users"></i> Contacts</a></li>
+	<li><a href="<?= Radix::link('/contact?kind=companies') ?>"><i class="fa fa-building"></i> Companies</a></li>
+	<li><a href="<?= Radix::link('/contact?kind=vendors') ?>"><i class="fa fa-truck"></i> Vendors</a></li>
+	<li><a href="<?= Radix::link('/contact/labels') ?>"><i class="fa fa-file-text-o"></i> Mail Labels</a></li>
+	<li><hr></li>
+	<li><a href="<?= Radix::link('/contact/import') ?>"><i class="fa fa-cloud-upload"></i> Import</a></li>
+	<li><a href="<?= Radix::link('/contact/sync') ?>"><i class="fa fa-refresh"></i> Sync</a></li>
+	</ul>
 </li>
-<?php
-// Workorders
-echo '<li><a href="' . Radix::link('/workorder') . '"><i class="fa fa-clock-o"></i> Work Orders</a>';
-    echo '<ul>';
-    if (!empty($_ENV['workorder']['id'])) {
-        echo '<li><a href="' . Radix::link('/workorder/view?w=' . $_ENV['workorder']['id']) . '">&laquo; Work Order #' . $this->WorkOrder->id . '</a></li>';
+
+<!-- Work Orders -->
+<li class="nav-item dropdown-hook">
+	<a class="nav-link" href="<?= Radix::link('/workorder') ?>"><i class="fa fa-clock-o"></i> Work Orders</a>
+	<ul class="dropdown-menu">
+	<?php
+	if (!empty($_ENV['workorder']['id'])) {
+		echo '<li><a href="' . Radix::link('/workorder/view?w=' . $_ENV['workorder']['id']) . '">&laquo; Work Order #' . $this->WorkOrder->id . '</a></li>';
 
         // Add Item
         echo '<li><a class="ajax-edit" data-name="woi-edit" href="' . Radix::link('/workorder/item?w=' . $this->WorkOrder->id) . '"><i class="fa fa-plus-square"></i> Add Item</a></li>';
@@ -117,13 +174,15 @@ echo '<li><a href="' . Radix::link('/workorder') . '"><i class="fa fa-clock-o"><
     echo '<li><a href="' . Radix::link('/workorder/report') . '"><i class="fa fa-bar-chart"></i> Reports</a></li>';
     // echo '<li><a href="' . Radix::link('/workorder/index/project' . img('/silk/1.3/table_lightning.png','List Projects') . ' Projects</a></li>';
     //$menu[] = array('/service_orders/index/project',img('/silk/1.3/table_lightning.png','List Projects').' Projects');
-    echo '</ul>';
-echo '</li>';
+	?>
+	</ul>
+</li>
 
-// Invoices
-echo '<li><a href="' . Radix::link('/invoice') . '"><i class="fa fa-usd"></i> Invoices</a>';
-    echo '<ul>';
-
+<!-- Invoices -->
+<li class="nav-item dropdown-hook">
+	<a class="nav-link" href="<?= Radix::link('/invoice') ?>"><i class="fa fa-usd"></i> Invoices</a>
+	<ul class="dropdown-menu">
+	<?php
     if (!empty($_ENV['invoice']['id'])) {
         echo '<li><a href="' . Radix::link('/invoice/view?i=' . $_ENV['invoice']['id']) . '"><i class="fa fa-usd"></i> &laquo;Invoice #' . $_ENV['invoice']['id'] . '</a></li>';
         echo '<li><a href="' . Radix::link('/invoice/pdf?i=' . $_ENV['invoice']['id']) . '"><i class="fa fa-file-pdf-o"></i> Printable</a></li>';
@@ -140,17 +199,20 @@ echo '<li><a href="' . Radix::link('/invoice') . '"><i class="fa fa-usd"></i> In
     // echo '<li><a href="' . Radix::link('/invoice/filter/past_due') . '">' , img('/silk/1.3/money_add.png','Past Due').' Past Due</a></li>';
     // echo '<li><a href="' . Radix::link('/invoice/filter/paid') . '">' , img('/silk/1.3/money_add.png','Paid').' Paid</a></li>';
     echo '<li><a href="' . Radix::link('/invoice/report') . '"><i class="fa fa-bar-chart"></i> Reports</a></li>';
-    echo '</ul>';
-echo '</li>';
+	?>
+	</ul>
+</li>
 
-// Accounting
-echo '<li><a href="' . Radix::link('/account') . '"><i class="fa fa-bar-chart"></i> Accounts</a>';
-    echo '<ul>';
+<!-- Accounts -->
+<li class="nav-item dropdown-hook">
+	<a class="nav-link" href="<?= Radix::link('/account') ?>"><i class="fa fa-bar-chart"></i> Accounts</a>
+	<ul class="dropdown-menu">
+	<?php
     echo '<li><a href="' . Radix::link('/account/transaction?id=-1') . '"><i class="fa fa-plus-square"></i> Transaction</a></li>';
     echo '<li><hr /></li>';
     echo '<li><a href="' . Radix::link('/account/edit') . '"><i class="fa fa-plus-square"></i> New Account</a></li>';
     // echo '<li><a href="' . Radix::link('/account/cheque') . '"><i class="fa fa-bar-chart"></i> Cheque</a></li>';
-//    echo '<li><a href="' . Radix::link('/account.wizard') . '"><i class="fa fa-magic"></i> Wizard</a></li>';
+    echo '<li><a href="' . Radix::link('/account.wizard') . '"><i class="fa fa-magic"></i> Wizard</a></li>';
     echo '<li><a href="' . Radix::link('/account/reconcile') . '"><i class="fa fa-refresh"></i> Reconcile</a></li>';
     echo '<li><a href="' . Radix::link('/account/close') . '"><i class="fa fa-flag-checkered"></i> Close Period</a></li>';
 
@@ -165,42 +227,42 @@ echo '<li><a href="' . Radix::link('/account') . '"><i class="fa fa-bar-chart"><
     echo '<li><a href="' . Radix::link('/account/report/equity') . '"><i class="fa fa-list-ol"></i> Owner Equity</a></li>';
 
     // echo '<li><a href="' . Radix::link('/account/tax-form') . '">Tax Schedules</a></li>';
-    echo '</ul>';
-echo '</li>';
+?>
+	</ul>
+</li>
 
+<?php
 // Plugins
 // @todo Google Link-Up - Plugin Menu/Config?
-$list = array();
-if (!empty($_ENV['google']['apps_domain'])) {
-    // @note Think These should Link Out to Google Services
-    $x = $_ENV['google']['apps_domain'];
-    $list[] = '<li><a href="https://mail.google.com/a/' . $x . '" target="_blank">Google Mail</a></li>';
-    $list[] = '<li><a href="https://www.google.com/calendar/hosted/' . $x . '" target="_blank">Google Calendar</a></li>';
-    $list[] = '<li><a href="https://docs.google.com/a/' . $x . '" target="_blank">Google Documents</a></li>';
-    // $list[] = sprintf('<a href="https://mail.google.com/tasks/a/' . $x . '/m" target="_blank"> Tasks</a>',$x);
-    $list[] = '<li><a href="https://mail.google.com/tasks/a/' . $x . '/ig" target="_blank">Google Tasks</a></li>';
-}
-if (!empty($_ENV['plugin'])) {
-    foreach ($_ENV['plugin'] as $k=>$v) {
-        $list[] = '<li><a href="' . Radix::link('/plugin?p=' . $k) .'">' . $k . '</a></li>';
-        // echo '<li><a href="/plugin?p=' . $_ENV[$v]['link'] . '">' . $k . '</a></li>';
-    }
-}
-if (count($list)) {
-    echo '<li><a href="#"><span>' . img('/tango/24x24/categories/preferences-system.png') . ' Plugins</span></a>';
-    echo '<ul>' . implode('',$list) . '</ul>';
-    echo '</li>';
-}
+//$list = array();
+//if (!empty($_ENV['google']['apps_domain'])) {
+//    // @note Think These should Link Out to Google Services
+//    $x = $_ENV['google']['apps_domain'];
+//    $list[] = '<li><a href="https://mail.google.com/a/' . $x . '" target="_blank">Google Mail</a></li>';
+//    $list[] = '<li><a href="https://www.google.com/calendar/hosted/' . $x . '" target="_blank">Google Calendar</a></li>';
+//    $list[] = '<li><a href="https://docs.google.com/a/' . $x . '" target="_blank">Google Documents</a></li>';
+//    // $list[] = sprintf('<a href="https://mail.google.com/tasks/a/' . $x . '/m" target="_blank"> Tasks</a>',$x);
+//    $list[] = '<li><a href="https://mail.google.com/tasks/a/' . $x . '/ig" target="_blank">Google Tasks</a></li>';
+//}
+//if (!empty($_ENV['plugin'])) {
+//    foreach ($_ENV['plugin'] as $k=>$v) {
+//        $list[] = '<li><a href="' . Radix::link('/plugin?p=' . $k) .'">' . $k . '</a></li>';
+//        // echo '<li><a href="/plugin?p=' . $_ENV[$v]['link'] . '">' . $k . '</a></li>';
+//    }
+//}
+//if (count($list)) {
+//    echo '<li><a href="#"><span>' . img('/tango/24x24/categories/preferences-system.png') . ' Plugins</span></a>';
+//    echo '<ul>' . implode('',$list) . '</ul>';
+//    echo '</li>';
+//}
 
-// Search Form
-echo '<li>';
-echo '<form action="' . Radix::link('/search') . '" method="get">';
-echo '<input id="q" name="q" placeholder="Search" value="" />';
-echo '</form></li>';
-echo '</ul>';
 ?>
-</nav>
+</ul>
 
-<nav class="two">
-	<i class="fa fa-gears"></i>
+<form class="form-inline" action="<?= Radix::link('/search') ?>" method="get">
+	<input class="form-control mr-sm-2" id="q" name="q" placeholder="Search" type="text" value="" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+</form>
+
+</div> <!-- /.collapse .navbar-collapse -->
 </nav>
--- theme/html.php
+++ theme/html.php
@@ -20,20 +20,24 @@ if (is_array($_ENV['title'])) {
 	$_ENV['title'] = implode(' &raquo; ',$_ENV['title']);
 }
 
-echo "<!DOCTYPE html>\n<html>\n";
+echo "<!DOCTYPE html>\n<html lang=\"en\">\n";
 echo '<head>';
 echo '<meta charset="utf-8">';
 echo '<meta name="viewport" content="initial-scale=1, user-scalable=yes">';
-//echo '<link href="' . Radix::link('/lib/reset-css/reset.css') . '" rel="stylesheet">';
-echo '<link href="' . Radix::link('/lib/HTML5-Reset/assets/css/reset.css') . '" rel="stylesheet">';
-echo '<link href="' . Radix::link('/lib/font-awesome/css/font-awesome.min.css') . '" rel="stylesheet">';
-// echo '<link href="//gcdn.org/jquery-ui/1.10.2/smoothness.css" rel="stylesheet">';
-// echo '<link href="//gcdn.org/radix/radix.css" rel="stylesheet">';
+echo '<meta name="HandheldFriendly" content="True">';
+echo '<meta name="MobileOptimized" content="320">';
+echo '<meta name="apple-mobile-web-app-capable" content="yes">'; // Install to Home Screen on iOS
+echo '<meta name="mobile-web-app-capable" content="yes">'; // Android to Home Screen
+echo '<meta name="theme-color" content="#212121">'; // Android Browser Titlebar Background
+echo '<meta name="viewport" content="initial-scale=1, user-scalable=yes">';
+
+echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />';
+echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha256-m/h/cUDAhf6/iBRixTbuc8+Rg2cIETQtPcH9D3p2Kg0=" crossorigin="anonymous" />';
 echo '<link href="' . Radix::link('/css/app.css') . '" rel="stylesheet">';
-echo '<link href="' . Radix::link('/img/imperium-icon.ico') . '" rel="shortcut icon">';
-echo '<link href="' . Radix::link('/img/imperium-icon.png') . '" rel="apple-touch-icon">';
-echo '<script src="' . Radix::link('/lib/jquery/dist/jquery.min.js') . '"></script>';
-echo '<script src="' . Radix::link('/lib/jquery-ui/jquery-ui.min.js') . '"></script>';
+//echo '<link href="' . Radix::link('/img/imperium-icon.ico') . '" rel="shortcut icon">';
+//echo '<link href="' . Radix::link('/img/imperium-icon.png') . '" rel="apple-touch-icon">';
+//echo '<script src="' . Radix::link('/lib/jquery/dist/jquery.min.js') . '"></script>';
+//echo '<script src="' . Radix::link('/lib/jquery-ui/jquery-ui.min.js') . '"></script>';
 // echo '<script src="//gcdn.org/jquery-ui/1.10.2/jquery-ui.js"></script>';
 // echo '<script src="' . Radix::link('/js/imperium.js') . '"></script>';
 // echo '<script>Imperium.base = "' . Radix::base(true) . '";</script>';
@@ -41,15 +45,7 @@ echo '<title>Imperium: ' . $_ENV['title'] . '</title>';
 echo "</head>\n<body>\n";
 
 // Content Header
-echo '<header>';
-$menu = Radix::block('menu.php');
-if (!empty($menu)) {
-    echo '<div id="menu">';
-    echo $menu;
-    echo '</div>';
-	// echo ImperiumView::mruDraw();
-}
-echo '</header>';
+echo Radix::block('menu.php');
 
 if (!empty($_ENV['h1'])) {
 	echo '<h1>' . $_ENV['h1'] . '</h1>';
@@ -76,6 +72,10 @@ echo '</div>'; // #core
 <footer>
 	<a href="http://imperium.edoceo.com">Imperium</a> &#169; 2001-2016 <a href="http://edoceo.com/">Edoceo, Inc</a>
 </footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha256-DiWJXXyq81WlPRnDfGmgYZj2aOVCKyEdJ1l+2TmDuAs=" crossorigin="anonymous"></script>
 
 </body>
 </html>
