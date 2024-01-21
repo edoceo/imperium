a2f915baeedd79b7d7ca9e59b11cc571ad0393a0
edoceo <code@edoceo.com>
2024-01-21 18:56:04 +0000

    BS5

--- view/invoice/item.php
+++ view/invoice/item.php
@@ -12,35 +12,74 @@
 
 namespace Edoceo\Imperium;
 
+use Edoceo\Radix;
 use Edoceo\Radix\HTML\Form;
 
-$q = Form::number('quantity', $this->InvoiceItem['quantity'], array('size'=>8));
-$r = Form::number('rate', $this->InvoiceItem['rate'], array('size'=>8));
-$u = Form::select('unit', $this->InvoiceItem['unit'], $this->UnitList);
+if (empty($this->InvoiceItem)) {
+	echo '<p class="fail">Failed to load a Invoice Item</p>';
+	return;
+}
 
 ?>
 
-<form method="post">
-<div class="pure-g">
-	<div class="pure-u-1-5"><div class="l">Kind:</div></div>
-	<div class="pure-u-1-5"><?= Form::select('kind', $this->InvoiceItem['kind'], InvoiceItem::$kind_list) ?></div>
-	<div class="pure-u-1-5"><div class="l">Date:</div></div>
-	<div class="pure-u-2-5"><?= Form::date('date', $this->InvoiceItem['date'], array('id'=>'woi_date','size'=>12)) ?></div>
+<form action="<?= Radix::link('/workorder/item?' . http_build_query( [ 'id' => $this->InvoiceItem['id'] ])) ?>" id="invoice-item-form" method="post">
 
-	<div class="pure-u-1-5"><div class="l">Name:</div></div>
-	<div class="pure-u-4-5"><td colspan="3"><?= Form::text('name', $this->InvoiceItem['name']) ?></div>
+<div class="row">
+<div class="col-md-6">
+	<div class="input-group mb-2">
+		<div class="input-group-text">Date:</div>
+		<?= Form::date('date', $this->InvoiceItem['date'], [ 'class' => 'form-control'] ) ?>
+	</div>
+</div>
+<div class="col-md-6">
+	<div class="input-group mb-2">
+		<div class="input-group-text">Kind:</div>
+		<?= Form::select('kind', $this->InvoiceItem['kind'], InvoiceItem::$kind_list, [ 'class' => 'form-control' ]) ?>
+	</div>
+</div>
+</div>
 
-	<div class="pure-u-1-5"><div class="l">Note:</div></div>
-	<div class="pure-u-4-5"><textarea name="note"><?= html($this->InvoiceItem['note']) ?></textarea></div>
+<div class="input-group mb-2">
+	<div class="input-group-text">Name:</div>
+	<input autofocus class="form-control" name="name" value="<?= __h($this->InvoiceItem['name']) ?>">
+</div>
 
-	<div class="pure-u-1-5"><div class="l">Cost:</div></div>
-	<div class="pure-u-2-5"><?= $q ?> <strong>@</strong> <?=$r ?> <strong>per</strong> <?=$u?></div>
-	<div class="pure-u-1-5"><div class="l">Tax:</div></div>
-	<div class="pure-u-1-5"><input maxlength="6" name="tax_rate" size="5" step="0.001" type="number" value="<?= tax_rate_format($this->InvoiceItem['tax_rate']) ?>">%</div>
+<div class="input-group mb-2">
+	<div class="input-group-text">Note:</div>
+	<textarea class="form-control" name="note"><?= __h($this->InvoiceItem['note']) ?></textarea>
+</div>
 
-	<div class="pure-u-1-5"><div class="l" title="Input and email address here and a notification email will be sent">Notify:</div></div>
-	<div class="pure-u-4-5"><?= Form::text('notify', $this->WorkOrderItem['notify']) ?></div>
+<div class="row">
+<div class="col-md-3">
+	<div class="input-group mb-2">
+		<div class="input-group-text">Quantity:</div>
+		<?= Form::number('quantity', $this->InvoiceItem['quantity'], [ 'class' => 'form-control' ]) ?>
+	</div>
+</div>
+<div class="col-md-3">
+	<div class="input-group mb-2">
+		<div class="input-group-text">Rate:</div>
+		<?= Form::number('rate', $this->InvoiceItem['rate'], [ 'class' => 'form-control'] ) ?>
+	</div>
+</div>
+<div class="col-md-3">
+	<div class="input-group mb-2">
+		<div class="input-group-text">per</div>
+		<?= Form::select('unit', $this->InvoiceItem['unit'], $this->UnitList, [ 'class' => 'form-control']); ?>
+	</div>
+</div>
+<div class="col-md-3">
+	<div class="input-group mb-2">
+		<div class="input-group-text">Tax:</div>
+		<input class="form-control" maxlength="6" name="tax_rate" size="5" step="0.001" type="number" value="<?= tax_rate_format($this->InvoiceItem['tax_rate']) ?>">
+		<div class="input-group-text">%</div>
+	</div>
+</div>
+</div>
 
+<div class="input-group mb-2">
+	<div class="input-group-text" title="Input an email address here and a notification email will be sent">Notify:</div>
+	<?= Form::text('notify', $this->InvoiceItem['notify'], [ 'class' => 'form-control' ]) ?>
 </div>
 
 <?php
@@ -64,9 +103,9 @@ $u = Form::select('unit', $this->InvoiceItem['unit'], $this->UnitList);
 // Buttons
 echo '<div class="cmd">';
 echo '<input name="invoice_id" type="hidden" value="' . $this->Invoice['id'] . '">';
-echo '<button class="good" name="a" type="submit" value="save">Save</button>';
+echo '<button class="btn btn-primary" name="a" type="submit" value="save">Save</button>';
 if (!empty($this->InvoiceItem['id'])) {
-    echo '<button class="fail" name="a" type="submit" value="delete">Delete</button>';
+    echo '<button class="btn btn-danger" name="a" type="submit" value="delete">Delete</button>';
 }
 echo '</div>';
 
@@ -83,8 +122,6 @@ echo '</form>';
 
 <script>
 $(function() {
-	$('#quantity').focus();
-
 	$('input[type=number]').on('blur', function() {
 		toNumeric(this);
 	});
