3357c9499a69346abd8f1d57c787763a52eb6254
edoceo <code@edoceo.com>
2024-01-28 17:52:08 +0000

    cleanup menu

--- block/menu.php
+++ block/menu.php
@@ -13,10 +13,6 @@
     @todo Make this much smarter, link to something with
 */
 
-// echo '<li><a href="' . Radix::link('/task/edit">' . '<i class="far fa-plus-square"></i> Add Task') . ' Task</a></li>';
-// echo '<li><a href="' . Radix::link('/alert/edit">' . '<i class="far fa-plus-square"></i> New Alert') . ' New Alert</a></li>';
-// echo '<li><a href="' . Radix::link('/timer/edit') . '"><i class="far fa-clock"></i> New Timer</a></li>';
-// echo '<li><a href="' . Radix::link('/calendar') . '"> Calendar</a></li>';
 // echo '<li><a href="' . Radix::link('/manual/'') . ' . $this->controller . '/' . $this->action . '"><img alt="Annotated Users Manual" src="' . Radix::link('/img/silk/help.png" /> Manual</a></li>';
 // echo '<li><a href="' . Radix::link('/contact/export') . '">Export</a></li>';
 
@@ -31,31 +27,10 @@ if (!ACL::may('/block/menu')) {
 ?>
 
 <style>
-nav li {
-	position: relative;
-}
-
 li.dropdown-hook:active > ul,
 li.dropdown-hook:focus > ul,
 li.dropdown-hook:hover > ul {
 	display: block;
-}
-
-ul.dropdown-menu {
-	/* border: 1px outset #000; */
-	display: none;
-	left: 0;
-	margin: 0;
-	padding: 0;
-	min-width: 15em;
-	position: absolute;
-	top: 98%;
-	z-index: 20;
-}
-ul.dropdown-menu a {
-	display: block;
-	padding: 0.125em 0.5em;
-	white-space: nowrap;
 }
 </style>
 
@@ -74,9 +49,27 @@ ul.dropdown-menu a {
 
 <div class="collapse navbar-collapse" id="menu0">
 
+<div class="me-2">
+<form action="<?= Radix::link('/search') ?>" autocomplete="off" method="get">
+	<div class="input-group input-group-sm">
+		<input autocomplete="off" 	class="form-control" id="q" name="q" placeholder="Search" type="text" value="" aria-label="Search">
+		<button class="btn btn-outline-success" type="submit">Search</button>
+	</div>
+</form>
+</div>
+
 <ul class="navbar-nav me-auto">
-<li class="nav-item">
-<!--
+<li class="nav-item dropdown-hook">
+	<a class="nav-link">Tools <i class="fa-solid fa-screwdriver-wrench"></i></a>
+	<ul class="dropdown-menu">
+		<li><a class="dropdown-item" href="<?= Radix::link('/task/edit') ?>"><i class="far fa-plus-square"></i> Add Task</a></li>
+		<li><a class="dropdown-item" href="<?= Radix::link('/alert/edit') ?>"><i class="far fa-plus-square"></i> New Alert</a></li>
+		<li><a class="dropdown-item" href="<?= Radix::link('/timer/edit') ?>"><i class="far fa-clock"></i> New Timer</a></li>
+		<li><a class="dropdown-item" href="<?= Radix::link('/calendar') ?>"> Calendar</a></li>
+		<li><a class="dropdown-item" href="<?= Radix::link('/note') ?>"><i class="far fa-sticky-note"></i> Notes</a></li>
+		<li><a class="dropdown-item" href="<?= Radix::link('/file') ?>"><i class="far fa-file-alt"></i> Files</a></li>
+	</ul>
+	<!--
 	<ul>
 	<?php
 	// Show MRU
@@ -94,11 +87,6 @@ ul.dropdown-menu a {
 	<li><hr></li>
 	<?php
 	// Somehow get the list of possible Status and use that to build a list here, colours as well.
-	echo '<li><a href="' . Radix::link('/note') . '"><i class="far fa-sticky-note"></i> Notes</a></li>';
-	echo '<li><a href="' . Radix::link('/file') . '"><i class="far fa-file-alt"></i> Files</a></li>';
-	echo '<li><hr /></li>';
-	echo '<li><a href="' . Radix::link('/settings') . '"><i class="far fa-cogs"></i> Settings</a></li>';
-	echo '<li><hr /></li>';
 	?>
 	</ul>
 -->
@@ -110,9 +98,9 @@ ul.dropdown-menu a {
 	<ul class="dropdown-menu">
 	<?php
 	if (!empty($_ENV['contact']['id'])) {
-		echo '<li><a href="' . Radix::link('/contact/view?c=' . $_ENV['contact']['id']) . '"><i class="far fa-user"></i> ' . $_ENV['contact']['name'] . '</a></li>';
+		echo '<li><a class="dropdown-item" href="' . Radix::link('/contact/view?c=' . $_ENV['contact']['id']) . '"><i class="far fa-user"></i> ' . $_ENV['contact']['name'] . '</a></li>';
 		echo '<li><hr /></li>';
-		echo '<li><a href="' . Radix::link('/contact/channel?a=create&amp;c=' . $_ENV['contact']['id']) . '"><i class="far fa-plus-square"></i> Add Channel</a></li>';
+		echo '<li><a class="dropdown-item" href="' . Radix::link('/contact/channel?a=create&amp;c=' . $_ENV['contact']['id']) . '"><i class="far fa-plus-square"></i> Add Channel</a></li>';
 		// echo '<li><a href="' . Radix::link('/task/edit/link/' . $this->Contact->link() . '"><i class="far fa-plus-square"></i> Add Task</a></li>';
 		// echo '<li><a href="' . Radix::link('/history/edit/link/' . $this->Contact->link() . '"><i class="far fa-plus-square"></i> Add History</a></li>';
 		//$menu[] = array('/timer/edit',$html->image('clock_add.png').' Clean');
@@ -122,14 +110,14 @@ ul.dropdown-menu a {
 		echo '<li><hr /></li>';
 	}
 	?>
-	<li><a href="<?= Radix::link('/contact/view') ?>"><i class="far fa-plus-square"></i> Create</a></li>
-	<li><a href="<?= Radix::link('/contact?kind=contacts')  ?>"><i class="fas fa-users"></i> Contacts</a></li>
-	<li><a href="<?= Radix::link('/contact?kind=companies') ?>"><i class="fas fa-building"></i> Companies</a></li>
-	<li><a href="<?= Radix::link('/contact?kind=vendors') ?>"><i class="fas fa-truck"></i> Vendors</a></li>
-	<li><a href="<?= Radix::link('/contact/labels') ?>"><i class="fas fa-mail-bulk"></i> Mail Labels</a></li>
+	<li><a class="dropdown-item" href="<?= Radix::link('/contact/view') ?>"><i class="far fa-plus-square"></i> Create</a></li>
+	<li><a class="dropdown-item" href="<?= Radix::link('/contact?kind=contacts')  ?>"><i class="fas fa-users"></i> Contacts</a></li>
+	<li><a class="dropdown-item" href="<?= Radix::link('/contact?kind=companies') ?>"><i class="fas fa-building"></i> Companies</a></li>
+	<li><a class="dropdown-item" href="<?= Radix::link('/contact?kind=vendors') ?>"><i class="fas fa-truck"></i> Vendors</a></li>
+	<li><a class="dropdown-item" href="<?= Radix::link('/contact/labels') ?>"><i class="fas fa-mail-bulk"></i> Mail Labels</a></li>
 	<li><hr></li>
-	<li><a href="<?= Radix::link('/contact/import') ?>"><i class="fas fa-upload"></i> Import</a></li>
-	<li><a href="<?= Radix::link('/contact/sync') ?>"><i class="fas fa-sync"></i> Sync</a></li>
+	<li><a class="dropdown-item" href="<?= Radix::link('/contact/import') ?>"><i class="fas fa-upload"></i> Import</a></li>
+	<li><a class="dropdown-item" href="<?= Radix::link('/contact/sync') ?>"><i class="fas fa-sync"></i> Sync</a></li>
 	</ul>
 </li>
 
@@ -137,14 +125,9 @@ ul.dropdown-menu a {
 <li class="nav-item dropdown-hook">
 	<a class="nav-link" href="<?= Radix::link('/workorder') ?>"><i class="fas fa-clipboard-list"></i> Work Orders</a>
 	<ul class="dropdown-menu">
+	<li><a class="dropdown-item" href="<?= Radix::link('/workorder/new') ?>"><i class="far fa-plus-square"></i> Create</a></li>
+	<li><a class="dropdown-item" href="<?= Radix::link('/workorder/report') ?>"><i class="fas fa-chart-bar"></i> Reports</a></li>
 	<?php
-    if ($_ENV['contact']['id']) {
-        echo '<li><a href="' . Radix::link('/workorder/new?c=' . $_ENV['contact']['id']) . '"><i class="far fa-plus-square"></i> Create</a></li>';
-    } else {
-        echo '<li><a href="' . Radix::link('/workorder/new') . '"><i class="far fa-plus-square"></i> Create</a></li>';
-    }
-
-    echo '<li><a href="' . Radix::link('/workorder/report') . '"><i class="fas fa-chart-bar"></i> Reports</a></li>';
     // echo '<li><a href="' . Radix::link('/workorder/index/project' . '<i class="far fa-list-alt"></i> Projects</a></li>';
     //$menu[] = array('/service_orders/index/project', '<i class="far fa-list-alt"></i> Projects');
 	?>
@@ -156,22 +139,12 @@ ul.dropdown-menu a {
 	<a class="nav-link" href="<?= Radix::link('/invoice') ?>"><i class="fas fa-file-invoice-dollar"></i> Invoices</a>
 	<ul class="dropdown-menu">
 	<?php
-    if (!empty($_ENV['invoice']['id'])) {
-        echo '<li><a href="' . Radix::link('/invoice/view?i=' . $_ENV['invoice']['id']) . '"><i class="fas fa-file-invoice-dollar"></i> &laquo;Invoice #' . $_ENV['invoice']['id'] . '</a></li>';
-        echo '<li><a href="' . Radix::link('/invoice/pdf?i=' . $_ENV['invoice']['id']) . '"><i class="fas fa-print"></i> Printable</a></li>';
-        echo '<li><hr /></li>';
-    }
-
-    // Create Invoice
-    if ($_ENV['contact']['id']) {
-        echo '<li><a href="' . Radix::link('/invoice/new?c=' . $_ENV['contact']['id']) . '"><i class="far fa-plus-square"></i> Create</a></li>';
-    } else {
-        echo '<li><a href="' . Radix::link('/invoice/new') . '"><i class="far fa-plus-square"></i> Create</a></li>';
-    }
+	// Create Invoice
+	echo '<li><a class="dropdown-item" href="' . Radix::link('/invoice/new') . '"><i class="far fa-plus-square"></i> Create</a></li>';
     // echo '<li><a href="' . Radix::link('/invoice/filter/active') . '">' , '<i class="fas fa-money-check-alt"></i> Active').' Active</a></li>';
     // echo '<li><a href="' . Radix::link('/invoice/filter/past_due') . '">' , '<i class="fas fa-money-check-alt"></i> Past Due').' Past Due</a></li>';
     // echo '<li><a href="' . Radix::link('/invoice/filter/paid') . '">' , '<i class="fas fa-money-check-alt"></i> Paid').' Paid</a></li>';
-    echo '<li><a href="' . Radix::link('/invoice/report') . '"><i class="fas fa-chart-bar"></i> Reports</a></li>';
+    echo '<li><a class="dropdown-item" href="' . Radix::link('/invoice/report') . '"><i class="fas fa-chart-bar"></i> Reports</a></li>';
 	?>
 	</ul>
 </li>
@@ -181,23 +154,23 @@ ul.dropdown-menu a {
 	<a class="nav-link" href="<?= Radix::link('/account') ?>"><i class="fas fa-chart-bar"></i> Accounts</a>
 	<ul class="dropdown-menu">
 	<?php
-    echo '<li><a href="' . Radix::link('/account/transaction?id=-1') . '"><i class="far fa-plus-square"></i> Transaction</a></li>';
+    echo '<li><a class="dropdown-item" href="' . Radix::link('/account/transaction?id=-1') . '"><i class="far fa-plus-square"></i> Transaction</a></li>';
     echo '<li><hr /></li>';
-    echo '<li><a href="' . Radix::link('/account/edit') . '"><i class="far fa-plus-square"></i> New Account</a></li>';
+    echo '<li><a class="dropdown-item" href="' . Radix::link('/account/edit') . '"><i class="far fa-plus-square"></i> New Account</a></li>';
     // echo '<li><a href="' . Radix::link('/account/cheque') . '"><i class="fas fa-chart-bar"></i> Cheque</a></li>';
-    echo '<li><a href="' . Radix::link('/account.wizard') . '"><i class="fas fa-magic"></i> Wizard</a></li>';
-    echo '<li><a href="' . Radix::link('/account/reconcile') . '"><i class="fas fa-sync-alt"></i> Reconcile</a></li>';
-    echo '<li><a href="' . Radix::link('/account/close') . '"><i class="fas fa-flag-checkered"></i> Close Period</a></li>';
+    echo '<li><a class="dropdown-item" href="' . Radix::link('/account.wizard') . '"><i class="fas fa-magic"></i> Wizard</a></li>';
+    echo '<li><a class="dropdown-item" href="' . Radix::link('/account/reconcile') . '"><i class="fas fa-sync-alt"></i> Reconcile</a></li>';
+    echo '<li><a class="dropdown-item" href="' . Radix::link('/account/close') . '"><i class="fas fa-flag-checkered"></i> Close Period</a></li>';
 
     echo '<li><hr /></li>';
-    echo '<li><a href="' . Radix::link('/account/report/income') . '"><i class="fas fa-funnel-dollar"></i>  Income (P&amp;L)</a></li>';
-    echo '<li><a href="' . Radix::link('/account/report/balance-sheet') . '"><i class="fas fa-balance-scale"></i> Balance Sheet</a></li>';
-    echo '<li><a href="' . Radix::link('/account/report/cash-flow') . '"><i class="fas fa-search-dollar"></i> Cash Flow</a></li>';
-    echo '<li><a href="' . Radix::link('/account/report/retained-earnings') . '"><i class="fas fa-piggy-bank"></i> Retained Earnings</a></li>';
+    echo '<li><a class="dropdown-item" href="' . Radix::link('/account/report/income') . '"><i class="fas fa-funnel-dollar"></i>  Income (P&amp;L)</a></li>';
+    echo '<li><a class="dropdown-item" href="' . Radix::link('/account/report/balance-sheet') . '"><i class="fas fa-balance-scale"></i> Balance Sheet</a></li>';
+    echo '<li><a class="dropdown-item" href="' . Radix::link('/account/report/cash-flow') . '"><i class="fas fa-search-dollar"></i> Cash Flow</a></li>';
+    echo '<li><a class="dropdown-item" href="' . Radix::link('/account/report/retained-earnings') . '"><i class="fas fa-piggy-bank"></i> Retained Earnings</a></li>';
 
     echo '<li><hr /></li>';
-    echo '<li><a href="' . Radix::link('/account/report/trial-balance') . '">Trial Balance</a></li>';
-    echo '<li><a href="' . Radix::link('/account/report/equity') . '">Owner Equity</a></li>';
+    echo '<li><a class="dropdown-item" href="' . Radix::link('/account/report/trial-balance') . '">Trial Balance</a></li>';
+    echo '<li><a class="dropdown-item" href="' . Radix::link('/account/report/equity') . '">Owner Equity</a></li>';
 
     // echo '<li><a href="' . Radix::link('/account/tax-form') . '">Tax Schedules</a></li>';
 ?>
@@ -232,17 +205,9 @@ ul.dropdown-menu a {
 ?>
 </ul>
 
-<div class="mx-2">
-<form action="<?= Radix::link('/search') ?>" autocomplete="off" method="get">
-	<div class="input-group input-group-sm">
-		<input autocomplete="off" autofocus class="form-control" id="q" name="q" placeholder="Search" type="text" value="" aria-label="Search">
-		<button class="btn btn-outline-success" type="submit">Search</button>
-	</div>
-</form>
-</div>
-
 <ul class="navbar-nav">
-	<li><a class="nav-link" href="<?= Radix::link('/auth/shut') ?>"><i class="fas fa-sign-out-alt"></i></a></li>
+	<li><a class="nav-link" href="<?= Radix::link('/settings') ?>"><i class="fa-solid fa-gears"></i> Settings</a></li>
+	<li><a class="btn btn-outline-danger" href="<?= Radix::link('/auth/shut') ?>"><i class="fas fa-sign-out-alt"></i></a></li>
 </ul>
 
 </div> <!-- /.collapse .navbar-collapse -->
